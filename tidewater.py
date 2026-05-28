6411cb43a86d09cee023a349f4c566022017ac1c
Hi237271 <68664925+Hi237271@users.noreply.github.com>
2026-05-28 18:12:26 -0400

    Updated tidewater.py

--- tidewater.py
+++ tidewater.py
@@ -52,25 +52,3 @@ with open("tidewater.csv",'w') as file:
     df = pd.read_json(json.dumps(csv_data,indent=4))
     df.to_csv("tidewater.csv",encoding='utf-8')
     df.to_excel('tidewater.xlsx')
-
-
-
-
-# for val in list(filter(lambda a: a!='\n', l.contents[1:])):
-#     try:
-#         val_data = []
-#         for auction in val.select('.card'):
-#             bid = auction.select_one('li').get_text()
-#             val_data.append(
-#                 {
-#                     'address':auction.select_one('.card-title').get_text(),
-#                     'info':auction.select_one('.card-text').get_text(),
-#                     'bid': bid[13:len(bid)]
-#                 }
-#             )
-#         title = val.contents[1].get_text().strip()
-#         data[title[12:len(title)] if title[0]!='T' else 'Today'] = val_data
-#     except Exception as e:
-#         print(e)
-# with open('out.json','w') as file:
-#     file.write(json.dumps(data,indent=4, sort_keys=True))
